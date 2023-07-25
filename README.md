1fdcf07deb024281f64cf6f6396994683c0af85c
James Tan <tan_yu_kai_james@s2022.ssts.edu.sg>
2023-07-25 12:41:24 +0800

    README and changes to colour

---
+++ README.md
@@ -0,0 +1,21 @@
+# Starling Flight Pattern Simulation
+
+## Contents
+
+### Starlings
+---
+Starlings are depicted as "*" and flock together
+
+### Obstacles
+---
+Obstacles are depicted as "X" and are lethal to Starlings
+
+### Attraction Points
+---
+Attraction Points are depicted as "+" and will attract Starlings
+
+### Predator
+---
+Predator are depicted as "PP
+                          PP" and will chase Starlings
+
--- flightpattern.py
+++ flightpattern.py
@@ -4,15 +4,15 @@ import curses
 import time
 
 # New screen dimensions (bigger size)
-WIDTH, HEIGHT = 150, 50
+WIDTH, HEIGHT = 120, 40
 
 # Starlings' properties
 NUM_STARLINGS = 150
-STARLING_SPEED = 1
-STARLING_TURN_ANGLE = math.radians(30)
+STARLING_SPEED = 2
+STARLING_TURN_ANGLE = math.radians(45)
 
 # Predator's properties
-PREDATOR_SPEED = 2.0
+PREDATOR_SPEED = 2.5
 PREDATOR_DAMAGE = 5
 NUM_PREDATORS = 1
 
@@ -21,11 +21,11 @@ NUM_OBSTACLES = 30
 
 # Attraction points' properties
 NUM_ATTRACTION_POINTS = 5
-ATTRACTION_STRENGTH = 0.01
+ATTRACTION_STRENGTH = 0.001
 
 # Starling health properties
 STARLING_STARTING_HEALTH = 100
-STARLING_HEALTH_DECAY_RATE = 0.2
+STARLING_HEALTH_DECAY_RATE = 0
 
 # Function to calculate the angle between two points
 def angle_between_points(p1, p2):
@@ -125,137 +125,131 @@ class Obstacle:
         self.x = x
         self.y = y
 
-# Attraction point class
+# AttractionPoint class
 class AttractionPoint:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-# Function to spawn new starlings
-def spawn_starlings():
-    num_to_spawn = int(NUM_STARLINGS * 0.7)
-    for _ in range(num_to_spawn):
-        new_starling = Starling(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1))
-        starlings.append(new_starling)
+# Function to display the predator
+def display_predator(stdscr, x, y):
+    try:
+        stdscr.addstr(int(y), int(x), 'PP', curses.color_pair(3))
+        stdscr.addstr(int(y) + 1, int(x), 'PP', curses.color_pair(3))
+    except curses.error:
+        pass
 
-# Function to spawn new predators
-def spawn_predators():
-    new_predator = Predator(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1))
-    predators.append(new_predator)
+def main(stdscr):
+    global starlings, predators, obstacles, attraction_points  # Declare all variables as global
 
-# Create starlings with updated initial positions
-starlings = [Starling(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_STARLINGS)]
+    # Initialize curses
+    curses.curs_set(0)  # Hide the cursor
+    stdscr.nodelay(1)   # Non-blocking input
+    stdscr.timeout(100) # Set a small timeout for getch()
 
-# Create obstacles with updated initial positions
-obstacles = [Obstacle(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_OBSTACLES)]
+    # Set up color pairs
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Starlings: Green on Black
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)   # Attraction points: Blue on Black
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)    # Predators: Red on Black
 
-# Create attraction points with updated initial positions
-attraction_points = [AttractionPoint(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_ATTRACTION_POINTS)]
+    # Create the initial objects (starlings, obstacles, etc.)
+    starlings = [Starling(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_STARLINGS)]
+    obstacles = [Obstacle(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_OBSTACLES)]
+    attraction_points = [AttractionPoint(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_ATTRACTION_POINTS)]
+    predators = [Predator(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_PREDATORS)]
 
-# Create predator list with updated initial positions
-predators = [Predator(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)) for _ in range(NUM_PREDATORS)]
+    try:
+        last_starling_spawn_time = time.time()
+        last_predator_spawn_time = time.time()
 
-# Initialize curses
-stdscr = curses.initscr()
-curses.curs_set(0)  # Hide the cursor
-stdscr.nodelay(1)   # Non-blocking input
-stdscr.timeout(100) # Set a small timeout for getch()
-curses.start_color()
-curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)    # Define color pair for starlings
-curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Define color pair for predator
+        # Main loop
+        while True:
+            stdscr.clear()
 
-try:
-    last_starling_spawn_time = time.time()
-    last_predator_spawn_time = time.time()
+            # Check if it's time to spawn new starlings
+            current_time = time.time()
+            if current_time - last_starling_spawn_time >= 5:
+                for _ in range(int(NUM_STARLINGS * 0.7)):
+                    starlings.append(Starling(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)))
+                last_starling_spawn_time = current_time
 
-    # Main loop
-    while True:
-        stdscr.clear()
+            # Check if it's time to spawn a new predator
+            if current_time - last_predator_spawn_time >= 10:
+                predators.append(Predator(random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)))
+                last_predator_spawn_time = current_time
 
-        # Check if it's time to spawn new starlings
-        current_time = time.time()
-        if current_time - last_starling_spawn_time >= 5:
-            spawn_starlings()
-            last_starling_spawn_time = current_time
-
-        # Check if it's time to spawn a new predator
-        if current_time - last_predator_spawn_time >= 10:
-            spawn_predators()
-            last_predator_spawn_time = current_time
-
-        # Move and display the predators
-        for predator in predators:
-            predator.move()
-            try:
-                stdscr.addch(int(predator.y), int(predator.x), 'P', curses.color_pair(2))
-            except curses.error:
-                pass  # Ignore error when adding a character outside the screen
-
-        # Get the average direction of nearby starlings for alignment behavior
-        for starling in starlings:
-            neighbors = [s for s in starlings if s != starling and distance_between_points((starling.x, starling.y), (s.x, s.y)) < 15]
-            starling.align(neighbors)
-
-            # Avoid obstacles and predator
-            for obstacle in obstacles:
-                if distance_between_points((starling.x, starling.y), (obstacle.x, obstacle.y)) < 6:
-                    target_angle = angle_between_points((starling.x, starling.y), (obstacle.x, obstacle.y))
-                    starling.steer_towards(target_angle + math.pi)
-
-                    # Reduce health when colliding with an obstacle
-                    starling.health -= 5
-
+            # Move and display the predators
             for predator in predators:
-                if distance_between_points((starling.x, starling.y), (predator.x, predator.y)) < 4:
-                    target_angle = angle_between_points((starling.x, starling.y), (predator.x, predator.y))
-                    starling.steer_towards(target_angle + math.pi)
+                predator.move()
+                display_predator(stdscr, predator.x, predator.y)
 
-                    # Reduce health when caught by the predator
-                    starling.health -= PREDATOR_DAMAGE
+            # Get the average direction of nearby starlings for alignment behavior
+            for starling in starlings:
+                neighbors = [s for s in starlings if s != starling and distance_between_points((starling.x, starling.y), (s.x, s.y)) < 15]
+                starling.align(neighbors)
 
-            # Attraction to points
+                # Avoid obstacles and predator
+                for obstacle in obstacles:
+                    if distance_between_points((starling.x, starling.y), (obstacle.x, obstacle.y)) < 6:
+                        target_angle = angle_between_points((starling.x, starling.y), (obstacle.x, obstacle.y))
+                        starling.steer_towards(target_angle + math.pi)
+
+                        # Reduce health when colliding with an obstacle
+                        starling.health -= 5
+
+                for predator in predators:
+                    if distance_between_points((starling.x, starling.y), (predator.x, predator.y)) < 4:
+                        target_angle = angle_between_points((starling.x, starling.y), (predator.x, predator.y))
+                        starling.steer_towards(target_angle + math.pi)
+
+                        # Reduce health when caught by the predator
+                        starling.health -= PREDATOR_DAMAGE
+
+                # Attraction to points
+                for attraction_point in attraction_points:
+                    target_angle = angle_between_points((starling.x, starling.y), (attraction_point.x, attraction_point.y))
+                    starling.steer_towards(target_angle)
+
+            # Move the starlings
+            for starling in starlings:
+                starling.move()
+
+            # Remove dead starlings
+            starlings = [starling for starling in starlings if starling.health > 0]
+
+            # Display the obstacles
+            for obstacle in obstacles:
+                if 0 <= int(obstacle.y) < HEIGHT and 0 <= int(obstacle.x) < WIDTH:
+                    try:
+                        stdscr.addch(int(obstacle.y), int(obstacle.x), 'X')
+                    except curses.error:
+                        pass  # Ignore error when adding a character outside the screen
+
+            # Display the attraction points
             for attraction_point in attraction_points:
-                target_angle = angle_between_points((starling.x, starling.y), (attraction_point.x, attraction_point.y))
-                starling.steer_towards(target_angle)
+                if 0 <= int(attraction_point.y) < HEIGHT and 0 <= int(attraction_point.x) < WIDTH:
+                    try:
+                        stdscr.addch(int(attraction_point.y), int(attraction_point.x), '+', curses.color_pair(2))
+                    except curses.error:
+                        pass
 
-        # Move the starlings
-        for starling in starlings:
-            starling.move()
-
-        # Remove dead starlings
-        starlings = [starling for starling in starlings if starling.health > 0]
-
-        # Display the obstacles
-        for obstacle in obstacles:
-            if 0 <= int(obstacle.y) < HEIGHT and 0 <= int(obstacle.x) < WIDTH:
+            # Display the current positions of starlings
+            for starling in starlings:
                 try:
-                    stdscr.addch(int(obstacle.y), int(obstacle.x), 'X')
+                    stdscr.addch(int(starling.y), int(starling.x), '*', curses.color_pair(1))
                 except curses.error:
-                    pass  # Ignore error when adding a character outside the screen
+                    pass
 
-        # Display the attraction points
-        for attraction_point in attraction_points:
-            if 0 <= int(attraction_point.y) < HEIGHT and 0 <= int(attraction_point.x) < WIDTH:
-                try:
-                    stdscr.addch(int(attraction_point.y), int(attraction_point.x), '+')
-                except curses.error:
-                    pass  # Ignore error when adding a character outside the screen
+            stdscr.refresh()
+            time.sleep(0.05)  # Adjust the sleep duration for animation speed
 
-        # Display the current positions of starlings
-        for starling in starlings:
-            try:
-                stdscr.addch(int(starling.y), int(starling.x), '*', curses.color_pair(1))
-            except curses.error:
-                pass  # Ignore error when adding a character outside the screen
+            # Check for user input (quit the animation with 'q')
+            ch = stdscr.getch()
+            if ch == ord('q'):
+                break
 
-        stdscr.refresh()
-        time.sleep(0.05)  # Adjust the sleep duration for animation speed
+    finally:
+        # Clean up curses
+        curses.endwin()
 
-        # Check for user input (quit the animation with 'q')
-        ch = stdscr.getch()
-        if ch == ord('q'):
-            break
-
-finally:
-    # Clean up curses
-    curses.endwin()
+curses.wrapper(main)
